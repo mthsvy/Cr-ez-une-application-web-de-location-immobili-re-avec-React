e11c77e1c52c057faab958faeeebf2926204e90a
Mathias <mthsvilly@gmail.com>
2023-08-31 09:30:59 +0200

    Changement icone de l'onglet

--- kasa/src/index.js
+++ kasa/src/index.js
@@ -11,7 +11,7 @@ import { createGlobalStyle } from "styled-components";
 import "./styles/global.scss";
 
 const LayoutStyle = createGlobalStyle``;
-
+document.title = "Kasa";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>

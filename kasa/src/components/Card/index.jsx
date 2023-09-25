e93bc5b596f2c8a26fa96581834ffd8436794f9d
Mathias <mthsvilly@gmail.com>
2023-09-25 11:21:03 +0200

    Rectification de la largeur des cards

--- kasa/src/components/Card/index.jsx
+++ kasa/src/components/Card/index.jsx
@@ -5,10 +5,10 @@ import { Link } from "react-router-dom";
 function Card({ cover, title, logement }) {
   return (
     <Link to={`/fiche-de-logement/${logement}`}>
-      <article className="card">
+      <div className="card">
         <img src={cover} alt="" />
         <span>{title}</span>
-      </article>
+      </div>
     </Link>
   );
 }
